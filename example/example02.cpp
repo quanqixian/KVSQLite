a6c4682e5a1e22f9a5c72a46681722a36361e4ee
Q_uan <quan_qixian@163.com>
2023-04-09 23:04:44 +0800

    add WriteBatch example

---
+++ example/example02.cpp
@@ -0,0 +1,37 @@
+#include "KVSQLite/db.h"
+#include <string>
+#include <iostream>
+
+void example01()
+{
+    KVSQLite::DB<std::string, std::string> * pDB = nullptr;
+    KVSQLite::Status status = KVSQLite::DB<std::string, std::string>::open(KVSQLite::Options(), "KVSQLite.db", &pDB);
+    if(!status.ok())
+    {
+        std::cout << status.toString() << std::endl;
+        return;
+    }
+
+    do
+    {
+        KVSQLite::WriteBatch<std::string, std::string> batch;
+
+        batch.put("key1", "val1");
+        batch.put("key2", "val2");
+        batch.del("key3");
+        if(!status.ok())
+        {
+            std::cout << status.toString() << std::endl;
+            break;
+        }
+
+    }while(0);
+
+    delete pDB;
+}
+
+int main(int argc, const char *argv[])
+{
+    example01();
+    return 0;
+}
