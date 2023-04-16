6acd0d5a8dcbac910521dc83b91909908994e0d7
Q_uan <quan_qixian@163.com>
2023-04-16 23:49:07 +0800

    change EXPORT macro

--- include/KVSQLite/Export.h
+++ include/KVSQLite/Export.h
@@ -1,18 +1,26 @@
 #ifndef KVSQLITE_EXPORT_H_
 #define KVSQLITE_EXPORT_H_
 
-#ifdef _WIN32
-    /*
-     * The KVSQLITE_EXPORT_SYMBOL macro is defined in the Cmake file that
-     * compiles the KVSQLite library 
-     */
-    #ifdef KVSQLITE_EXPORT_SYMBOL 
-        #define KVSQLITE_EXPORT    __declspec( dllexport )
+#ifdef SQLITE_SHARED_LIBRARY
+    #ifdef _WIN32
+        /*
+         * The KVSQLITE_EXPORT_SYMBOL macro is defined in the Cmake file that
+         * compiles the KVSQLite library 
+         */
+        #ifdef KVSQLITE_EXPORT_SYMBOL 
+            #define KVSQLITE_EXPORT    __declspec( dllexport )
+        #else
+            #define KVSQLITE_EXPORT    __declspec( dllimport )
+        #endif
     #else
-        #define KVSQLITE_EXPORT    __declspec( dllimport )
+        #ifdef KVSQLITE_EXPORT_SYMBOL 
+            #define KVSQLITE_EXPORT    __attribute__((visibility("default")))
+        #else 
+            #define KVSQLITE_EXPORT
+        #endif
     #endif
 #else
     #define KVSQLITE_EXPORT
 #endif
 
-#endif
+#endif /* end of KVSQLITE_EXPORT_H_ */
